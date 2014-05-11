954a8fff92f78c8c7a3c152a39b9458334d93589
Jason <jason@pixeljet.net>
2014-05-11 21:36:09 +1000

    Add classpath generation helper

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
+if [ $1="cp" ]; then
+mvn -Dmdep.outputFile=classpath.txt dependency:build-classpath
+fi
+
 while :
     do
-      jjs -fx -cp target/classes:target/dependency/* src/main/javascript/admin.js
+      jjs -fx -cp target/classes:$(cat classpath.txt) src/main/javascript/admin.js
       sleep 1
     done
